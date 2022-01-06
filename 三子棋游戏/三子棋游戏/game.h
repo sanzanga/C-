1347912265ca2bbf79392a834d06f7627c6535e8
sanzang <3201401355@qq.com>
2022-01-06 18:09:45 +0800

    三子棋游戏

---
+++ 三子棋游戏/三子棋游戏/game.h
@@ -0,0 +1,19 @@
+#define ROW 3
+#define COL 3
+#include <stdlib.h>//srand
+#include <time.h>
+
+
+//声明
+void InitBoard(char board[ROW][COL], int row, int col);
+void DisplayBoard(char board[ROW][COL], int row, int col);
+void PlayerMove(char board[ROW][COL], int row, int col);
+void ComputerMove(char board[ROW][COL], int row, int col);
+
+
+//告诉我们四种游戏状态
+//玩家赢  '*'
+//电脑赢  '#'
+//平局   'Q'
+//继续   'C'
+char IsWin(char board[ROW][COL], int row, int col);
