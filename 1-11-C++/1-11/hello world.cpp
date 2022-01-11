cc62f705f5e74bf77dd61ccca5949167f0dae515
sanzang <3201401355@qq.com>
2022-01-11 21:49:05 +0800

    第一天学习C++

---
+++ 1-11-C++/1-11/hello world.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main3()
+{
+	//Êä³öhello world
+	cout << "hello world" << endl;
+	system("pause");
+	return 0;
+}
+
---
+++ 1-11-C++/1-11/test.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+//1、单行注释
+
+//2、多行注释
+
+/*
+   main是一个函数的入口
+   每个程序都必须有这么一个函数
+   有且仅有一个
+*/
+
+
+int main1()
+{
+	//下面的函数的意义就是在屏幕上输出hello world
+	cout << "hello world" << endl;
+	system("pause");
+	return 0;
+}
