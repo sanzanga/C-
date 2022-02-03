343c0095a35f7d54e0ebea37f99d74d924a2faf3
sanzang <3201401355@qq.com>
2022-02-03 18:56:01 +0800

    C语言——练习

---
+++ 练习/练习/test.cpp
@@ -0,0 +1,112 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//#include <stdio.h>
+//int main()
+//{
+//	printf("Practice makes perfect!");
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	printf("v   v\n");
+//	printf(" v v\n");
+//	printf("  v\n");
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	printf("The size of short is %d bytes.\n", sizeof(short));
+//	printf("The size of int is %d bytes.\n", sizeof(int));
+//	printf("The size of long is %d bytes.\n", sizeof(long));
+//	printf("The size of long long is %d bytes.\n", sizeof(long long));
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	printf("     **     \n");
+//	printf("     **     \n");
+//	printf("************\n");
+//	printf("************\n");
+//	printf("    * *     \n");
+//	printf("    * *     \n");
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	printf("0%o ", 1234);
+//	printf("0X%x\n", 1234);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//    int a;
+//    scanf("%d", &a);
+//    a = 1234;
+//    printf("0%o 0X%X", a, a);
+//    return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	printf("%15d", 0XABCDEF);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int a = printf("Hello world!\n");
+//	printf("%d", a);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int a, b, c;
+//	scanf("%d%d%d", &a, &b, &c);
+//	printf("score1=%d,score2=%d,score3=%d", a, b, c);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int a;
+//	float b, c, d;
+//	scanf("%d;%f,%f,%f", &a, &b, &c, &d);
+//	printf("The each subject score of No. &d is %.2f, %.2f, %.2f.", a, b, c, d);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	char a;
+//	scanf("%c", &a);
+//	printf("      %c      \n", a);
+//	printf("    %c %c     \n", a, a);
+//	printf("   %c %c %c   \n", a, a, a);
+//	printf("  %c %c %c %c \n", a, a, a, a);
+//	printf("%c %c %c %c %c\n", a, a, a, a, a);
+//	return 0;
+//}
