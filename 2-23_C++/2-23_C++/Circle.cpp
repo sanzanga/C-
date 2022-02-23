c6f99b3f14feace5302a7dd92a615bddc43d59b1
sanzang <3201401355@qq.com>
2022-02-23 23:54:40 +0800

    C++——封装

---
+++ 2-23_C++/2-23_C++/Circle.cpp
@@ -0,0 +1,35 @@
+////圆类
+//class Circle
+//{
+//public:
+//
+//	//设置半径
+//	void setR(int r)
+//	{
+//		m_R = r;
+//	}
+//
+//	//获取半径
+//	int getR()
+//	{
+//		return m_R;
+//	}
+//
+//	//设置圆心
+//	void setCenter(Point center)
+//	{
+//		m_Center = center;
+//	}
+//
+//	//获取圆心
+//	Point getCenter()
+//	{
+//		return m_Center;
+//	}
+//
+//private:
+//	int m_R;//半径
+//
+//	//在类总可以让另一个类作为本来中的成员
+//	Point m_Center;//圆心
+//};
